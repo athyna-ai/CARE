a823e8f0223a1acc2c175fbac6fa5ef9a5c20483
athyna-ai <athenahannah02@gmail.com>
2025-10-14 15:45:40 +0800

    Clean up unnecessary files: test files, debug files, bat files, md files, and env.example

--- assets/start-server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Care CMS Development Server...
-echo.
-echo Server will be available at: http://localhost:8000
-echo Press Ctrl+C to stop the server
-echo.
-cd /d "%~dp0"
-php -S localhost:8000
-pause
